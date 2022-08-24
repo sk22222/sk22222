5ee31af7c0586fb7f5079656d5ca6267aec6baa7
sk22222 <101616712+sk22222@users.noreply.github.com>
2022-08-24 01:36:55 -0600

    Update Juniper Impact analysis check.py

--- Juniper Impact analysis check.py
+++ Juniper Impact analysis check.py
@@ -1,5 +1,12 @@
+# $language = "python"
+# $interface = "1.0"
+
+# This automatically generated script may need to be
+# edited in order to work correctly.
+
 def Main():
     crt.Screen.Synchronous = True
+    crt.Screen.Send("sh int description | match" + chr(9) + ' "vid|VID|stream|Stream|Streami|dvm|hdv|cdv|qpsk|_QPSK|QPSK|ois|sgt|sva|svp|VOB|vas|vdc|vds|vdx|vns|nsg|hvd|HVD|RFGW|apx|apex|STRM|str|TYPE=VID|VIDEO|SDV|sdv|NSG|XOD|DMM|VideoEdg|OUTPUT|DCM|OWNER=VIDEO|MGMT|l3mmt|Video_|Video-|DAC|Apex|,ARP|ARPD-MGMT|chtrav|l3vod|Monitori|Encoder|sen|,OM0|MGMT-OM|GMT-|qmm|VLS-|-VLS|VideoPublic| -qmm|l2adv|chrtav|chrtrc|,netg|TRK|DNCS|monitori|HED:|UMG|=PEER|M_REACH|3K" '+ chr(13))
     crt.Screen.Send("show ver | no-more"  + chr(13))
     crt.Screen.WaitForString(">")
     crt.Screen.Send("show conf | match OSPF | no-more"  + chr(13))
@@ -13,6 +20,5 @@ def Main():
     crt.Screen.Send( " show int desc | match up | except down | no-more" + chr(13))
     crt.Screen.WaitForString(">")
     crt.Screen.Send("show bgp summary | no-more" + chr(13))
-        
     
 Main()
